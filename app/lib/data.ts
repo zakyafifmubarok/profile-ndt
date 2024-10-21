2d1a3904dc9e80d7a83b5c5f3780c1243d725890
syandi <sandianugrah96@gmail.com>
2024-10-21 12:12:38 +0700

    Fetching Data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -36,7 +36,8 @@ export async function fetchLatestInvoices() {
       JOIN customers ON invoices.customer_id = customers.id
       ORDER BY invoices.date DESC
       LIMIT 5`;
-
+    
+      
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
