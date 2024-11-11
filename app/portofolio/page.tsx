adb598e15bfd94ed3bdc8ed8be88677129787187
syandi <sandianugrah96@gmail.com>
2024-11-11 15:41:19 +0700

    WIP contact

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -172,7 +172,7 @@ export default function PortofolioPage() {
       </div>
 
       {/* Main section */}
-      <div
+      <section
         ref={mainRef}
         className="z-10 relative overflow-hidden h-[90vh] animate-fade-in"
       >
@@ -197,10 +197,10 @@ export default function PortofolioPage() {
             <p className="tracking-widest lg:text-2xl lg:tracking-letter18">Digital Teknologi</p>
           </div>
         </div>
-      </div>
+      </section>
 
       {/* About section */}
-      <div
+      <section
         ref={aboutRef}
         className={`z-20 transform transition-opacity duration-1000 ${
           isAboutVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
@@ -230,10 +230,10 @@ export default function PortofolioPage() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
 
       {/* Vision section */}
-      <div
+      <section
         ref={vissionRef}
         className="relative overflow-hidden h-full max-h-[100vh] bg-gray-950 text-white"
       >
@@ -273,10 +273,10 @@ export default function PortofolioPage() {
             <li>Konsultasi IT, dll.</li>
           </div>
         </div>
-      </div>
+      </section>
 
       {/* Product section */}
-      <div
+      <section
         ref={productRef}
         className={`transform transition-opacity duration-1000 ${
           isProductVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
@@ -341,7 +341,50 @@ export default function PortofolioPage() {
             </div>
           </div>
         </div>
-      </div>
+      </section>
+      <section className="bg-white">
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+          <form action="#" className="space-y-8">
+            <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg "
+                  placeholder="name@flowbite.com"
+                  required
+                />
+            </div>
+            <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                <input
+                  type="text"
+                  id="subject"
+                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm"
+                  placeholder="Let us know how we can help you"
+                  required
+                />
+            </div>
+            <div className="sm:col-span-2">
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                <textarea
+                  id="message"
+                  rows={6}
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300"
+                  placeholder="Leave a comment..."
+                ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="py-3 px-5 text-sm font-medium text-center border rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800"
+              >Send message</button
+            >
+          </form>
+        </div>
+      </section>
       {/* CSS section */}
       <style jsx>{`
           .shine-text {
