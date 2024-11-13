3786e84dafadc8dad10e497a2757bec8d38dfe6b
syandi <sandianugrah96@gmail.com>
2024-11-13 10:15:09 +0700

    portofolio contact

--- app/portofolio/page.tsx
+++ app/portofolio/page.tsx
@@ -3,7 +3,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, FormEvent } from 'react';
 import { lusitana, poppins } from '@/app/ui/fonts'
 
 export default function PortofolioPage() {
@@ -154,11 +154,22 @@ export default function PortofolioPage() {
     };
   }, []);
 
+  const handleConsultationEmail = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget)
+    const email = 'cvnahcodadigitalteknologi@gmail.com'
+    const subject = formData.get('subject')
+    const message = formData.get('message')
+    
+    window.location.href = `mailto:${email}?subject=${subject}&body=${message}`;
+  }
+
   return (
     <main className='relative'>
       {/* Navbar section */}
       <div className={`fixed w-full opacity-80 top-0 z-50 py-2 px-4 text-white ${
-        isScrolled ? 'bg-gray-950' : 'bg-transparent'
+        isScrolled ? 'bg-slate-950' : 'bg-transparent'
       }`}>
         <div className='flex justify-between'>
           <div>
@@ -174,27 +185,21 @@ export default function PortofolioPage() {
       {/* Main section */}
       <section
         ref={mainRef}
-        className="z-10 relative overflow-hidden h-[90vh] animate-fade-in"
+        className='z-10 relative overflow-hidden h-[90vh] animate-fade-in'
       >
-        <div className='absolute size-full bg- inset-0 bg-gray-950 opacity-90'
-          style={{ transform: `translateY(${offset}px)` }}
-        >
-          <Image
-            src="/background/bg-1.png"
-            alt="Screenshots of the siplah.tokoladang.co.id"
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
         <div
-          className="z-10 absolute inset-0 bg-cover bg-center transition-transform duration-200 ease-linear"
+          className='absolute size-full bg-gradient-to-b from-slate-900 via-white to-slate-900'
+          style={{ transform: `translateY(${offset}px)` }}
+        />
+        <div
+          className='z-10 absolute inset-0 bg-cover bg-center transition-transform duration-200 ease-linear'
           style={{ transform: `translateY(${offset}px)` }}
         >
           <div className={`${lusitana.className} h-full flex flex-col justify-center text-center uppercase text-shadow`}>
-            <h1 className="text-5xl lg:text-9xl font-extrabold tracking-widest">
+            <h1 className='text-5xl lg:text-9xl font-extrabold tracking-widest'>
               Nahcoda
             </h1>
-            <p className="tracking-widest lg:text-2xl lg:tracking-letter18">Digital Teknologi</p>
+            <p className='tracking-widest lg:text-2xl lg:tracking-letter18'>Digital Teknologi</p>
           </div>
         </div>
       </section>
@@ -206,12 +211,12 @@ export default function PortofolioPage() {
           isAboutVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
         }`}
       >
-        <div className="bg-gradient-to-b to-white from-slate-300 relative overflow-hidden min-h-[40vh]">
-          <div className="max-w-7xl mx-auto py-20 lg:py-28 px-6">
-            <div className="flex gap-20">
+        <div className='bg-gradient-to-b to-white from-slate-300 relative overflow-hidden min-h-[40vh]'>
+          <div className='max-w-7xl mx-auto py-20 lg:py-28 px-6'>
+            <div className='flex gap-20'>
               <div className={`${poppins.className}`}>
-                <h1 className="text-4xl font-bold uppercase">About the Company</h1>
-                <p className="mt-4 text-xl text-justify">
+                <h1 className='text-4xl font-bold uppercase'>About the Company</h1>
+                <p className='mt-4 text-xl text-justify'>
                   CV Nahcoda Digital Teknologi adalah perusahaan teknologi informasi yang
                   berdiri sejak Tahun 2019 yang awalnya bernama PT Juru Ketik Nusantara,
                   kemudian pada Tahun 2022 bertransformasi menjadi CV Nahcoda Digital
@@ -221,12 +226,15 @@ export default function PortofolioPage() {
                   memberikan layanan terbaik kepada anda.
                 </p>
               </div>
-              <Image
-                src="/background/picture1.png"
-                width={500}
-                height={380}
-                alt="Screenshots of the tokoladang.co.id"
-              />
+              <div className='hidden lg:block'>
+                <Image
+                  src='/background/picture1.png'
+                  width={500}
+                  height={380}
+                  alt='Picture of CEO'
+                  style={{ width: 'auto', height: 'auto' }}
+                />
+              </div>
             </div>
           </div>
         </div>
@@ -235,20 +243,18 @@ export default function PortofolioPage() {
       {/* Vision section */}
       <section
         ref={vissionRef}
-        className="relative overflow-hidden h-full max-h-[100vh] bg-gray-950 text-white"
+        className='relative overflow-hidden h-full max-h-[100vh] bg-slate-950 text-white'
       >
-        <div className='absolute top-[300px] size-full inset-0 opacity-50'
+        <div className='absolute top-[150px] h-screen inset-0 opacity-50'
           style={{ transform: `translateY(${scrollY * -0.2}px)` }}
         >
           <Image
-            src="/background/spotlight.webp"
-            alt="Screenshots of the siplah.tokoladang.co.id"
-            layout="fill"
-            objectFit="cover"
+            src='/background/spotlight.webp'
+            alt='Screenshots of the siplah.tokoladang.co.id'
+            fill
+            style={{ objectFit: 'cover' }}
           />
         </div>
-        {/* <div className='max-w-7xl mx-auto w-full h-full bg-red-300'>
-        </div> */}
         <div className={`${poppins.className} max-w-4xl mx-auto py-40 px-6`}>
           <div
             className={`transform transition-transform duration-1000 ${
@@ -282,10 +288,10 @@ export default function PortofolioPage() {
           isProductVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
         }`}
       >
-        <div className="bg-gradient-to-b to-slate-300 from-white relative overflow-hidden min-h-[40vh]">
+        <div className='bg-gradient-to-b to-slate-300 from-white relative overflow-hidden min-h-[40vh]'>
           <div className='max-w-7xl mx-auto py-20 lg:py-28 px-6'>
             <h1 className='text-left text-4xl font-medium uppercase'>Our Product</h1>
-            <div className="mt-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+            <div className='mt-8 grid grid-cols-1 lg:grid-cols-2 gap-6'>
               <div className={`transform transition-opacity duration-1000 ${
                   isProductImageVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
                 }`}
@@ -297,10 +303,10 @@ export default function PortofolioPage() {
                   <p>(PT. Ladang Karya Husada)</p>
                 </div>
                 <Image
-                  src="/products/siplah.tokoladang.co.id.png"
+                  src='/products/siplah.tokoladang.co.id.png'
                   width={1000}
                   height={760}
-                  alt="Screenshots of the siplah.tokoladang.co.id"
+                  alt='Screenshots of the siplah.tokoladang.co.id'
                 />
               </div>
               <div className={`transform transition-opacity duration-1000 delay-500 ${
@@ -314,10 +320,10 @@ export default function PortofolioPage() {
                   <p>(PT. Ladang Karya Husada)</p>
                 </div>
                 <Image
-                  src="/products/tokoladang.co.id.png"
+                  src='/products/tokoladang.co.id.png'
                   width={1000}
                   height={760}
-                  alt="Screenshots of the tokoladang.co.id"
+                  alt='Screenshots of the tokoladang.co.id'
                 />
               </div>
               <div>
@@ -342,44 +348,35 @@ export default function PortofolioPage() {
           </div>
         </div>
       </section>
-      <section className="bg-white">
-        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Contact Us</h2>
-          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
-          <form action="#" className="space-y-8">
+      <section className='bg-gradient-to-b from-slate-50 via-white to-slate-50'>
+        <div className='py-8 lg:py-16 px-4 mx-auto max-w-screen-md'>
+          <h2 className='mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900'>Contact Us</h2>
+          <p className='mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl'>Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+          <form onSubmit={handleConsultationEmail} className='space-y-8'>
             <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+                <label className='block mb-2  text-sm font-medium'>Subject</label>
                 <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg "
-                  placeholder="name@flowbite.com"
+                  id='subject'
+                  name='subject'
+                  type='text'
+                  className='block p-3 w-full text-sm rounded-lg border border-slate-600 shadow-sm'
+                  placeholder='Let us know how we can help you'
                   required
                 />
             </div>
-            <div>
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                <input
-                  type="text"
-                  id="subject"
-                  className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm"
-                  placeholder="Let us know how we can help you"
-                  required
-                />
-            </div>
-            <div className="sm:col-span-2">
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+            <div className='sm:col-span-2'>
+                <label className='block mb-2 text-sm font-medium'>Your message</label>
                 <textarea
-                  id="message"
+                  id='message'
+                  name='message'
                   rows={6}
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300"
-                  placeholder="Leave a comment..."
+                  className='block p-2.5 w-full text-sm rounded-lg shadow-sm border border-slate-600'
+                  placeholder='Leave a comment...'
                 ></textarea>
             </div>
             <button
-              type="submit"
-              className="py-3 px-5 text-sm font-medium text-center border rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800"
+              type='submit'
+              className='py-3 px-5 text-sm font-medium text-center border border-slate-600 rounded-lg hover:bg-slate-50'
               >Send message</button
             >
           </form>
